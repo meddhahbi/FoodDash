1a01820466c5ad1e3b303e102e6194efe0c5ca1c
Mohamed Dhahbi <56275931+med850@users.noreply.github.com>
2023-06-20 08:49:02 +0200

    Make restaurant item screen

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import RestaurantItem from './src/Components/RestaurantItem';
+import restaurants from './assets/data/restaurants.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <RestaurantItem restaurant={restaurants[0]}/>
+      <RestaurantItem restaurant={restaurants[1]}/>
+
+   <StatusBar style="auto" />
     </View>
   );
 }
@@ -16,5 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:10
   },
 });
